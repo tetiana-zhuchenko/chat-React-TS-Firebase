1e303374f0b632ac380028252dc7090e0bb2b2bb
Tania <taniyhamail@gmail.com>
2024-03-29 09:00:30 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
